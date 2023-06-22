b25da6d3775d007d981f6710516378b5c87722c2
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2023-06-22 15:20:02 +0800

    changed data in chatroom from static to dynamic using flatmap

--- frontend/chatappui/src/components/ChatroomPage.js
+++ frontend/chatappui/src/components/ChatroomPage.js
@@ -3,6 +3,49 @@ import React from 'react'
 import '../styling/ChatroomPage.css'
 
 function ChatroomPage() {
+
+  var userA = {
+    name: "User A",
+    messages: [
+      {text:"Greetings everyone!"},
+    ]
+  }
+
+  var userB = {
+    name: "User B",
+    messages: [
+      {text:"Welcome User A!"},
+      {text:"I would like to visit the beach soon"},
+      {text: "And I won't forget to bring sunscreen"}
+    ]
+  }
+
+  var userC = {
+    name: "User C",
+    messages: [
+      {text:"I am looking forward to a good one"},
+    ]
+  }
+
+  var userD = {
+    name: "User D",
+    messages: [
+      {text:"Let's have a great summer"},
+      {text:"That's agreed User B"}
+    ]
+  }
+
+  const users = [userA, userB, userC, userD]
+
+  const renderMessage = users.flatMap((user, index) => (
+    user.messages.flatMap((message, messageIndex) => (
+          <li className='create--message' key={`${index}-${messageIndex}`}>
+            <span className='username'>{user.name}</span>
+            <span className='content'>{message.text}</span>
+          </li>
+    ))
+  ))
+
   return (
     <div className='chatroomPage--container'>
       <span className='active--users'>
@@ -17,34 +60,7 @@ function ChatroomPage() {
 
       <span className='chatroom'>
         <ul className='message--container'>
-          <li className='create--message'>
-            <span className='username'>User A:</span>
-            <span className='content'>Greetings everyone!</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User B:</span>
-            <span className='content'>Welcome User A!</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User D:</span>
-            <span className='content'>Let's have a great summer</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User C:</span>
-            <span className='content'>I am looking forward to a good one</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User B:</span>
-            <span className='content'>I would like to visit the beach soon</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User B:</span>
-            <span className='content'>And I won't forget to bring sunscreen</span>
-          </li>
-          <li className='create--message'>
-            <span className='username'>User D:</span>
-            <span className='content'>That's agreed User B</span>
-          </li>
+         {renderMessage}
         </ul>
       </span>
 
