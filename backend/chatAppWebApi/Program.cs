6150c73d7b96bde946de2c16a13c49ec7d854b11
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2023-06-22 18:37:50 +0800

    added backend folder and created web api files

---
+++ backend/chatAppWebApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(create =>
+{
+    create.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Rabbit Chat",
+        Description = "Chat and collaborate as you wish",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(create =>
+    {
+        create.SwaggerEndpoint("/swagger/v1/swagger.json", "Rabbit Chat API V1");
+    });
+}
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Rabbit Chat incoming!");
+
+
+app.Run();
