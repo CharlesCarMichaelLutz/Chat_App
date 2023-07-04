e018bb7d925545e47c2d8ba18847b708f92cfe61
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2023-07-04 10:45:00 +0800

    changed ChatroomModel to reflect accuracte data model I wanted for Users and Messages

--- backend/chatAppWebApi/Models/ChatroomModel.cs
+++ backend/chatAppWebApi/Models/ChatroomModel.cs
@@ -25,16 +25,18 @@
     public class ChatroomData
     {
         public UserModel[] UserModel { get; set; }
+        public MessageModel[] MessageModel { get; set; }
         
     }
     public class UserModel
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public MessageModel[] MessageModel { get; set; }
     }
     public class MessageModel
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
         public string Message { get; set; }
     }
 }
