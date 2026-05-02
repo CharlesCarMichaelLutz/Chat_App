8c6cdfdf7dcc8901823b7b1973a84d36359072b7
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-05-02 08:11:56 +0800

    created custom hook useLogin to simplify Login page

--- frontend/src/hooks/useLoginForm.js
+++ frontend/src/hooks/useLoginForm.js
@@ -1,9 +1,125 @@
 import React from 'react'
+import { useChat } from './useChat';
+import { useState } from 'react';
+import { baseApi } from '../api/base';
+import { useNavigate } from 'react-router-dom';
 
 const useLoginForm = () => {
-  return (
-    <div>useLoginForm</div>
-  )
+  const { setAuth, setIsLoggedIn } = useChat();
+  const [isSignUp, setIsSignUp] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userErrors, setUserErrors] = useState([]);
+  const [passErrors, setPassErrors] = useState([]);
+  const navigate = useNavigate();
+
+    const handleUsername = (e) => {
+      let usernameInput = e.target.value;
+      setUsername(usernameInput);
+    };
+  
+    const handlePassword = (e) => {
+      let passwordInput = e.target.value;
+      setPassword(passwordInput);
+    };
+
+    const handleGuestSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await baseApi.post("login", {
+        Username: import.meta.env.VITE_GUEST,
+        Password: import.meta.env.VITE_PASSWORD,
+      });
+
+      setAuth(response.data);
+      setIsLoggedIn(true);
+
+      if (response.status == 200) {
+        navigate("/chatroom");
+      }
+      clearInput();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    setUserErrors([]);
+    try {
+      const response = await baseApi.post("login", {
+        Username: username,
+        Password: password,
+      });
+
+      setAuth(response.data);
+      setIsLoggedIn(true);
+
+      if (response.status == 200) {
+        navigate("/chatroom");
+      }
+      clearInput();
+    } catch (error) {
+      console.log("response: ", error.response);
+      console.log("data: ", error.response.data);
+      if (error.status === 400) {
+        setUserErrors((prevErr) => [...prevErr, error.response.data]);
+      }
+    }
+  };
+
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault();
+    setUserErrors([]);
+    setPassErrors([]);
+    try {
+      const response = await baseApi.post("signup", {
+        Username: username,
+        Password: password,
+      });
+      setAuth(response.data);
+      setIsLoggedIn(true);
+
+      if (response.status == 200) {
+        navigate("/chatroom");
+      }
+      clearInput();
+    } catch (error) {
+      if (error.response.status === 400) {
+        const errorData = error.response?.data;
+        setUserErrors(errorData?.usernameErrors ?? []);
+        setPassErrors(errorData?.passwordErrors ?? []);
+      } else if (error.response.status === 404) {
+        setUserErrors((prevErr) => [...prevErr, error.response.data]);
+      }
+    }
+  };
+
+  const clearInput = () => {
+    setUsername("");
+    setPassword("");
+  };
+
+  const toggleSignUp = () => {
+    setIsSignUp((prev) => !prev);
+    setUserErrors([]);
+    setPassErrors([]);
+    clearInput();
+  };
+
+  return {    
+    handleGuestSubmit,
+    isSignUp,
+    handleRegisterSubmit,
+    username,
+    password,
+    handleUsername,
+    handlePassword,
+    userErrors,
+    passErrors,
+    handleLoginSubmit,
+    toggleSignUp
+  }
 }
 
 export default useLoginForm
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,115 +1,24 @@
 import RabbitImage from "../components/RabbitImage";
-import { useState } from "react";
-import { useChat } from "../hooks/useChat";
-import { useNavigate } from "react-router-dom";
-import { baseApi } from "../api/base";
 import GuestLoginForm from "../components/GuestLoginForm";
 import RegisterForm from "../components/RegisterForm";
 import LoginForm from "../components/LoginForm";
 import ToggleLoginFormButton from "../components/ToggleLoginFormButton";
+import useLoginForm from "../hooks/useLoginForm";
 
 export const Login = () => {
-  const { setAuth, setIsLoggedIn } = useChat();
-  const [isSignUp, setIsSignUp] = useState(true);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [userErrors, setUserErrors] = useState([]);
-  const [passErrors, setPassErrors] = useState([]);
-  const navigate = useNavigate();
-
-  const handleUsername = (e) => {
-    let usernameInput = e.target.value;
-    setUsername(usernameInput);
-  };
-
-  const handlePassword = (e) => {
-    let passwordInput = e.target.value;
-    setPassword(passwordInput);
-  };
-
-  const handleGuestSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await baseApi.post("login", {
-        Username: import.meta.env.VITE_GUEST,
-        Password: import.meta.env.VITE_PASSWORD,
-      });
-
-      setAuth(response.data);
-      setIsLoggedIn(true);
-
-      if (response.status == 200) {
-        navigate("/chatroom");
-      }
-      clearInput();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleLoginSubmit = async (e) => {
-    e.preventDefault();
-    setUserErrors([]);
-    try {
-      const response = await baseApi.post("login", {
-        Username: username,
-        Password: password,
-      });
-
-      setAuth(response.data);
-      setIsLoggedIn(true);
-
-      if (response.status == 200) {
-        navigate("/chatroom");
-      }
-      clearInput();
-    } catch (error) {
-      console.log("response: ", error.response);
-      console.log("data: ", error.response.data);
-      if (error.status === 400) {
-        setUserErrors((prevErr) => [...prevErr, error.response.data]);
-      }
-    }
-  };
-
-  const handleRegisterSubmit = async (e) => {
-    e.preventDefault();
-    setUserErrors([]);
-    setPassErrors([]);
-    try {
-      const response = await baseApi.post("signup", {
-        Username: username,
-        Password: password,
-      });
-      setAuth(response.data);
-      setIsLoggedIn(true);
-
-      if (response.status == 200) {
-        navigate("/chatroom");
-      }
-      clearInput();
-    } catch (error) {
-      if (error.response.status === 400) {
-        const errorData = error.response?.data;
-        setUserErrors(errorData?.usernameErrors ?? []);
-        setPassErrors(errorData?.passwordErrors ?? []);
-      } else if (error.response.status === 404) {
-        setUserErrors((prevErr) => [...prevErr, error.response.data]);
-      }
-    }
-  };
-
-  const clearInput = () => {
-    setUsername("");
-    setPassword("");
-  };
-
-  const toggleSignUp = () => {
-    setIsSignUp((prev) => !prev);
-    setUserErrors([]);
-    setPassErrors([]);
-    clearInput();
-  };
+  const {
+    handleGuestSubmit,
+    isSignUp,
+    handleRegisterSubmit,
+    username,
+    password,
+    handleUsername,
+    handlePassword,
+    userErrors,
+    passErrors,
+    handleLoginSubmit,
+    toggleSignUp,
+  } = useLoginForm();
 
   return (
     <>
